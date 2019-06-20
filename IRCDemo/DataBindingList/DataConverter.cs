9092d22b513beb6078ac722fd73f12caa9484f9e
Luka Radovanovic <lukadlm97@gmail.com>
2019-06-20 15:40:54 +0200

    Added Conversions od Date

---
+++ IRCDemo/DataBindingList/DataConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataBindingList
+{
+    public class DataConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, 
+            object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.ToString("d");
+        }
+
+        public object ConvertBack(object value, Type targetType, 
+            object parameter, CultureInfo culture)
+        {
+            string strValues = value as string;
+            DateTime resultDateTame;
+            if(DateTime.TryParse(strValues,out resultDateTame))
+            {
+                return resultDateTame;
+            }
+            throw new Exception("Unable to convert string to date time");
+        }
+    }
+}
---
+++ IRCDemo/DataConvertDemo/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConvertDemo
+{
+    class Employee
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value;OnPropertyChanged(); }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value;OnPropertyChanged(); }
+        }
+
+        private DateTime _startDate;
+
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value;OnPropertyChanged(); }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(
+            [CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+
+        public Employee(string name,string title,DateTime startDate)
+        {
+            Name = name;
+            Title = title;
+            StartDate = startDate;
+        }
+    }
+}
